a514e4ba61a4ce1875a53743d498644670132632
Nityanand <58543978+199-cmd@users.noreply.github.com>
2020-08-20 19:02:19 +0530

    Update main.c

--- main.c
+++ main.c
@@ -60,3 +60,74 @@ int main()
 //   x = x + y;
 //   return x;
 // }
+
+//pointer
+  //pointer_type *identifier;
+  // int j = 63;
+  // int *p = NULL;
+  // p = &j;
+  // printf("The address of j is %x\n", &j);
+  // printf("p containing address %x\n", p);
+  // printf("the value of j is %d\n", j);
+  // printf("p is pointing to the value %d\n", *p);
+
+  //pointer of array
+  // int a[5] = {22,33,44,55,66};
+  // int *p = NULL;
+  // int i;
+  // p = a;
+  // for(i=0;i<5;i++)
+  // {
+  //   printf("%d",*(p+i));
+  // }
+
+  //String in c 
+  // char str[6] = "Nitya"; 
+  // char str1[6] = {'N','I','T','\0'};
+  //   char name[25];
+  //   char city[20];
+  //   int age;
+  //   printf("Enter your name,city and age: ");
+  //   scanf("%s %s,%d",name,city,&age);
+
+
+// struct student
+// {
+//   int age;
+//   int grade;
+//   char name[25];
+// };
+// int main() 
+// {
+//   //variable declaration of student structure
+//   struct student s1;
+//   struct student s2;
+//   //assign the value
+//   s1.age = 21;
+//   s1.grade = 10;
+//   s2.age = 22;
+//   s2.grade = 9;
+//   //assign the string
+//   sprintf(s1.name,"Adityanand");
+//   sprintf(s2.name,"Nityanand");
+//   //printing the values
+//   printf("Student name is %s, age is %d and grade is %d \n",s1.name,s1.age,s1.grade);
+//   printf("Student name is %s, age is %d and grade is %d \n",s2.name,s2.age,s2.grade);
+   // #include <stdlib.h>
+// int main()
+// {
+// 	FILE *pf;
+// 	char name[25];
+// 	pf = fopen("file","w");
+// 	if(pf==NULL)
+// 	{
+// 		printf("Error!!Occur");
+// 		exit(1);
+// 	}
+// 	printf("Enter the name: ");
+// 	scanf("%s",name);
+// 	fprintf(pf,"%s\n",name);//for writing
+//  fscanf(pf,"%s\n",name);//for reading
+// 	// printf("%s\n",name);
+// 	fclose(pf);
+// 	return 0;
