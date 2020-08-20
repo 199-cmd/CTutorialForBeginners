345203247ba216804716a632e44a4e51585a54bb
Nityanand <58543978+199-cmd@users.noreply.github.com>
2020-08-20 18:50:17 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,62 @@
+//preprocessor directive
+//header files
+#include <stdio.h>
+//entry point of your program
+//prototype for compler
+// int sum(int x,int y);//actual parameter
+int main()
+{
+  //builtin function that display the mess
+  //printf("hello World\n");
+  //Datatypes in c
+  //int float double char
+  // printf("Size of integer %ld\n",sizeof(int));
+  // printf("Size of float %ld\n",sizeof(float));
+  // printf("Size of double %ld\n",sizeof(double));
+  // printf("Size of character %ld\n",sizeof(char));
+    // int num;
+    // printf("Enter the number: \n");
+    // scanf("%d", &num);
+    // printf("The number is %d",num);
+    // int num = 5;
+    // float f = 2.5;
+    // double d = 20.225;
+    // char c = 'N';
+    // printf("The integer is %d\n",num);
+    // printf("The float is %f\n",f);
+    // printf("The double is %lf\n",d);
+    // printf("The char is %c\n",c);
+    //COnstants
+    // const double pi = 3.14159;
+    // printf("%f \n", pi);
+    // Input and Output
+    // char c = getchar();
+    // printf("Your letter is %c \n",c);
+
+    // int num1,num2;
+    // printf("Enter the value: \n");
+    // scanf("%d %d", &num1, &num2);
+    // printf("Sum of number: %d \n",num1+num2);
+
+   //Operator in c
+  //  int num1,num2;
+  //  printf("Enter the value: ");
+  //  scanf("%d %d", &num1, &num2);
+  //  int result = num1 % num2;
+  //  printf("Result is %d \n",result);
+
+  //FUnction in c 
+  //declaration
+  // int x,y,result;
+  // x = 2;
+  // y = 4;
+  // result = sum(x, y);
+  // printf("Addition of the number is %d \n",result);
+  return 0;
+}
+//body
+// int sum(int x,int y)//formal parameter
+// {
+//   x = x + y;
+//   return x;
+// }
